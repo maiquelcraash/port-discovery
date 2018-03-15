198c74d03aa9c00f786567555985c39a335bda8d
maiquel <maiquel_craash@hotmail.com>
2018-03-15 10:52:01 -0300

    Test 3

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 3000;
+let publicIP = "????";
+
+const publicIp = require('public-ip');
 
 // Routes
 app.get('/', function(req, res) {
@@ -8,7 +11,8 @@ app.get('/', function(req, res) {
 		"message":"Welcome to Acml Server",
 		"local_port": port,
 		"headers": req.headers,
-		"rawheaders": req.rawheaders
+		"rawheaders": req.rawheaders,
+		"public_ip": publicIP;
 	});
 });
 
@@ -16,3 +20,9 @@ app.get('/', function(req, res) {
 
 app.listen(port);
 console.log('Listening on localhost:'+ port);
+
+publicIp.v4().then(ip => {
+	console.log(ip);
+	publicIP = ip;
+//=> '46.5.21.123'
+});
