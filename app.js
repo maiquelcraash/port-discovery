ee3971733a1b1f4029860e5517554eca0b6d5004
maiquel <maiquel_craash@hotmail.com>
2018-03-15 10:14:17 -0300

    Test 1

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // Routes
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.send({
+		"message":"Welcome to Acml Server",
+		"local_port": port,
+		"public_host": req.rawHeaders[1]
+	});
 });
 
 // Listen
-var port = process.env.PORT || 3000;
+
 app.listen(port);
 console.log('Listening on localhost:'+ port);
