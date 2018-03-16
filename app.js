0ee3e60603953fafc83c64ae51d045e3413f9e34
maiquel <maiquel_craash@hotmail.com>
2018-03-16 08:27:31 -0300

    Test 5 - With vs-stun

--- app.js
+++ app.js
@@ -18,11 +18,21 @@ app.get('/', function (req, res) {
 app.listen(port);
 console.log('Listening on localhost:' + port);
 
-let detecPort = setInterval(() => {
-	let run = exec('node_modules/pia/bin/pia pia -a --display-stun --specify-stun "stun.l.google.com:19302"', function (error, stdout, stderr) {
-		if (error) {
-			console.log(error, stderr);
-		}
-	});
-	console.log(run.toString());
-}, 5000);
+let stun = require('vs-stun');
+
+let socket, server = { host: 'stun.l.google.com', port: 19302 }
+
+let callback = function callback ( error, value ) {
+	if ( !error ) {
+		socket = value;
+
+		setInterval(()=>{
+			console.log(socket.stun);
+		}, 2000);
+
+		// socket.close();
+	}
+	else console.log('Something went wrong: ' + error);
+};
+
+stun.connect(server, callback);
